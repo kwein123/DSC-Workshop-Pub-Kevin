fa9b7c1139f408c4ae457700ef914901868153b7
Kevin Weinrich <Weinrich.Kevin@epa.gov>
2022-02-03 07:25:45 -0500

    BootHole re-push

---
+++ DSC/FixThese.ps1
@@ -0,0 +1,51 @@
+#.\push-DscTo.ps1 -computer a26267ridz002
+#.\push-DscTo.ps1 -computer z2626xfldz002
+#.\push-DscTo.ps1 -computer q26267mndz102
+#.\push-DscTo.ps1 -computer p2626xordz001
+#.\push-DscTo.ps1 -computer n26267gadz002
+#.\push-DscTo.ps1 -computer f2626cokdz001
+#.\push-DscTo.ps1 -computer n2626ugadz003
+#.\push-DscTo.ps1 -computer q2626xmndz101
+#.\push-DscTo.ps1 -computer a2626xridz001
+#.\push-DscTo.ps1 -computer l2626xordz001
+
+#.\push-DscTo.ps1 -computer h26267ohhh601
+#.\push-DscTo.ps1 -computer n26267gadz002
+#.\push-DscTo.ps1 -computer p26267orhh601
+#.\push-DscTo.ps1 -computer p2626xordz001
+#.\push-DscTo.ps1 -computer q26267mndz102
+#.\push-DscTo.ps1 -computer q2626xmndz101
+#.\push-DscTo.ps1 -computer v2626umchh503
+#.\push-DscTo.ps1 -computer z26267flhh501
+
+#.\push-DscTo.ps1 -computer q2626xmnec001
+#.\push-DscTo.ps1 -computer v26265ncay502
+#.\push-DscTo.ps1 -computer v26265ncay504
+#.\push-DscTo.ps1 -computer v26267mcpk506
+#.\push-DscTo.ps1 -computer v26267ncay506
+
+# After backups
+# .\push-DscTo.ps1 -computer z2626xfldz002
+
+#.\push-DscTo.ps1 -computer v2626urtec102
+#.\push-DscTo.ps1 -computer v2626umcec104
+#.\push-DscTo.ps1 -computer v2626umcay515
+#.\push-DscTo.ps1 -computer v2626umcay514
+#.\push-DscTo.ps1 -computer v26267ncsp520
+#.\push-DscTo.ps1 -computer v26267ncsp501
+#.\push-DscTo.ps1 -computer v26267ncpk606
+
+# Throws an error until manually run:
+#.\push-DscTo.ps1 -computer v26267nchh510
+
+#.\push-DscTo.ps1 -computer v26267ncay506
+
+# Not done?
+#.\push-DscTo.ps1 -computer v26267nchh001
+
+#.\push-DscTo.ps1 -computer v26267ncec201
+#.\push-DscTo.ps1 -computer v26267ncay520
+#.\push-DscTo.ps1 -computer v26267ncay516
+#.\push-DscTo.ps1 -computer v26267ncay510
+#.\push-DscTo.ps1 -computer v26267ncay508
+#.\push-DscTo.ps1 -computer v26267ncay507
