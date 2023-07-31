1bd4f7fd52cabbe6f99ec4261661406dca0297bd
Kevin Weinrich <weinrichkevin@gmail.com>
2023-07-31 13:30:12 -0400

    Pushed out slimmer configs to all servers

---
+++ DSC/doit.cmd
@@ -0,0 +1,27 @@
+rem del \\v26265ncay504\c$\boothole\_boothole.cmd
+del \\v26267ncay507\c$\boothole\_boothole.cmd
+del \\v26267ncay510\c$\boothole\_boothole.cmd
+del \\v26267ncpk526\c$\boothole\_boothole.cmd
+del \\v26267ncsp520\c$\boothole\_boothole.cmd
+del \\v26267ncsp524\c$\boothole\_boothole.cmd
+del \\a26267rihh601\c$\boothole\_boothole.cmd
+del \\a2626xridz001\c$\boothole\_boothole.cmd
+del \\n2626ugadz003\c$\boothole\_boothole.cmd
+del \\q2626xmnay001\c$\boothole\_boothole.cmd
+del \\q2626xmnay003\c$\boothole\_boothole.cmd
+del \\v26267ncpk612\c$\boothole\_boothole.cmd
+del \\v26267ncsp525\c$\boothole\_boothole.cmd
+
+rem powershell .\push-DscTo.ps1 -computer v26265ncay504
+powershell .\push-DscTo.ps1 -computer v26267ncay507
+powershell .\push-DscTo.ps1 -computer v26267ncay510
+powershell .\push-DscTo.ps1 -computer v26267ncpk526
+powershell .\push-DscTo.ps1 -computer v26267ncsp520
+powershell .\push-DscTo.ps1 -computer v26267ncsp524
+powershell .\push-DscTo.ps1 -computer a26267rihh601
+powershell .\push-DscTo.ps1 -computer a2626xridz001
+powershell .\push-DscTo.ps1 -computer n2626ugadz003
+powershell .\push-DscTo.ps1 -computer q2626xmnay001
+powershell .\push-DscTo.ps1 -computer q2626xmnay003
+powershell .\push-DscTo.ps1 -computer v26267ncpk612
+powershell .\push-DscTo.ps1 -computer v26267ncsp525
